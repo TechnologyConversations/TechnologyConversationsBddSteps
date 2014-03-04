8f1fb2f11134a95653df7c0effcb166769c25a58
vfarcic <viktor.farcic@gtech.com>
2014-03-04 18:26:05 +0100

    Web steps

--- src/main/java/com/technologyconversations/bdd/steps/WebSteps.java
+++ src/main/java/com/technologyconversations/bdd/steps/WebSteps.java
@@ -1,13 +1,14 @@
 package com.technologyconversations.bdd.steps;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
 import com.opera.core.systems.OperaDriver;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
@@ -15,13 +16,17 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.$;
 
 public class WebSteps {
 
-    public static void main(final String[] args) throws Exception {
-        WebSteps steps = new WebSteps();
-        steps.open("/login");
-    }
+    // TODO Add methods that use selector with index
+    // TODO Add methods that use findAll selector ($$)
+
+    private final String selectorsInfo = "\nAny CSS selector can be used to locate the element. Most commonly used selectors are:\n" +
+            ".myClass: Matches any element with class equal to 'myClass'\n" +
+            "#myId: Matches any element with ID equal to 'myId'\n" +
+            "For more information on CSS selectors please consult http://www.w3.org/TR/CSS21/selector.html.";
 
     /* Supported drivers are:
             firefox (default)
@@ -39,7 +44,7 @@ public class WebSteps {
         } else if ("chrome".equals(driver)) {
             WebDriverRunner.setWebDriver(new ChromeDriver());
         } else if ("htmlunit".equals(driver)) {
-            WebDriverRunner.setWebDriver(new HtmlUnitDriver());
+            WebDriverRunner.setWebDriver(new HtmlUnitDriver(true));
         } else if ("ie".equals(driver)) {
             WebDriverRunner.setWebDriver(new InternetExplorerDriver());
         } else if ("opera".equals(driver)) {
@@ -51,10 +56,41 @@ public class WebSteps {
         }
     }
 
+    // Given
+
+    @BddAnnotation(description = "Opens specified address.")
     @Given("Web address $url is opened")
     public void open(String url) {
         Selenide.open(url);
     }
+
+    @BddAnnotation(description = "Sets timeout used when operating with elements.")
+    @Given("Web timeout is $seconds seconds")
+    public void configTimeout(int seconds) {
+        Configuration.timeout = seconds * 1000;
+    }
+
+    // When
+
+    @BddAnnotation(description = "Clicks the element." + selectorsInfo)
+    @When("Web user clicks the element $selector")
+    public void clickElement(String selector) {
+        findElement(selector).click();
+    }
+
+    @BddAnnotation(description = "Clears the text field and sets the specified value." + selectorsInfo)
+    @When("Web user sets value $value to the element $selector")
+    public void setElementValue(String value, String selector) {
+        findElement(selector).setValue(value);
+    }
+
+    @BddAnnotation(description = "Appends the specified value." + selectorsInfo)
+    @When("Web user appends value $value to the element $selector")
+    public void appendElementValue(String value, String selector) {
+        findElement(selector).append(value);
+    }
+
+    // Then
 
     @BddAnnotation(description = "Verifies that the title of the current page is as expected.")
     @Then("Web page title is $title")
@@ -62,16 +98,35 @@ public class WebSteps {
         Assert.assertEquals(title, title());
     }
 
-    @BddAnnotation(description = "Clicks the element. Any CSS selector can be used to locate the element. Most commonly used selectors are:\n" +
-            ".myClass: Matches any element with class equal to 'myClass'\n" +
-            "#myId: Matches any element with ID equal to 'myId'\n" +
-            "For more information on CSS selectors please consult http://www.w3.org/TR/CSS21/selector.html.")
-    @When("Web user clicks the element $selector")
-    public void click(String selector) {
+    @BddAnnotation(description = "Verifies that the element text is the same as specified." + selectorsInfo)
+         @Then("Web element $selector should have text $text")
+         public void shouldHaveText(String selector, String text) {
+        $(selector).shouldHave(text(text));
+    }
+
+    @BddAnnotation(description = "Verifies that the element value is the same as specified." + selectorsInfo)
+    @Then("Web element $selector should have value $value")
+    public void shouldHaveValue(String selector, String value) {
+        $(selector).shouldHave(value(value));
+    }
+
+    @BddAnnotation(description = "Verifies that the element text is NOT the same as specified." + selectorsInfo)
+    @Then("Web element $selector should NOT have text $text")
+    public void shouldNotHaveText(String selector, String text) {
+        $(selector).shouldNotHave(text(text));
+    }
+
+    @BddAnnotation(description = "Verifies that the element value is NOT the same as specified." + selectorsInfo)
+    @Then("Web element $selector should NOT have value $value")
+    public void shouldNotHaveValue(String selector, String value) {
+        $(selector).shouldNotHave(value(value));
+    }
+
+    private SelenideElement findElement(String selector) {
         if (Character.isLetter(selector.charAt(0))) {
             selector = "#" + selector;
         }
-        $(selector).click();
+        return $(selector);
     }
 
 }
--- src/test/java/com/technologyconversations/bdd/steps/WebStepsTest.java
+++ src/test/java/com/technologyconversations/bdd/steps/WebStepsTest.java
@@ -1,13 +1,18 @@
 package com.technologyconversations.bdd.steps;
 
+import com.codeborne.selenide.ex.ElementNotFound;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.Date;
 
 public class WebStepsTest {
 
     private WebSteps steps;
+    private final String linkId = "#linkId";
+    private final String inputId = "#inputId";
 
     @Before
     public void before() {
@@ -16,25 +21,113 @@ public class WebStepsTest {
             steps.setDriver("htmlunit");
         }
         File file = new File("src/test/resources/index.html");
+        steps.configTimeout(0);
         steps.open("file:///" + file.getAbsolutePath());
     }
 
     @Test
+    public void configTimeoutShouldSetTimeout() {
+        Date start;
+        steps.configTimeout(0);
+        start = new Date();
+        try {
+            steps.clickElement("#nonExistingElement");
+        } catch (ElementNotFound e) { }
+        Assert.assertTrue((new Date().getTime() - start.getTime()) < 1000);
+        steps.configTimeout(1);
+        start = new Date();
+        try {
+            steps.clickElement("#nonExistingElement");
+        } catch (ElementNotFound e) { }
+        Assert.assertTrue((new Date().getTime() - start.getTime()) >= 1000);
+    }
+
+    @Test
     public void openShouldRetrieveSpecifiedUrlUsingIdSelector() {
-        steps.click("#linkId");
+        steps.clickElement(linkId);
         steps.checkTitle("BDD Steps Test Page");
     }
 
     @Test
     public void openShouldRetrieveSpecifiedUrlUsingCssSelector() {
-        steps.click(".linkClass");
+        steps.clickElement(".linkClass");
         steps.checkTitle("BDD Steps Test Page");
     }
 
     @Test
     public void openShouldUseIdAsSelectorIfSelectorStartsWithLetter() {
-        steps.click("linkId");
+        steps.clickElement("linkId");
         steps.checkTitle("BDD Steps Test Page");
+    }
+
+    @Test
+    public void shouldHaveTextShouldPassIfElementTextIsTheSame() {
+        steps.shouldHaveText(linkId, "This is link");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void shouldHaveTextShouldFailIfElementTextIsNotTheSame() {
+        steps.shouldHaveText(linkId, "This is non-existent text");
+    }
+
+    @Test
+    public void shouldHaveValueShouldPassIfElementTextIsTheSame() {
+        steps.shouldHaveValue(inputId, "This is input");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void shouldHaveValueShouldFailIfElementTextIsNotTheSame() {
+        steps.shouldHaveValue(inputId, "This is non-existent value");
+    }
+
+    @Test
+    public void shouldNotHaveTextShouldPassIfElementTextIsNotTheSame() {
+        steps.shouldNotHaveText(linkId, "This is non-existent text");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void shouldNotHaveTextShouldFailIfElementTextIsTheSame() {
+        steps.shouldNotHaveText(linkId, "This is link");
+    }
+
+    @Test
+    public void shouldNotHaveValueShouldPassIfElementTextIsNotTheSame() {
+        steps.shouldNotHaveValue(inputId, "This is non-existent text");
+    }
+
+    @Test(expected = AssertionError.class)
+    public void shouldNotHaveValueShouldFailIfElementTextIsTheSame() {
+        steps.shouldNotHaveValue(inputId, "This is input");
+    }
+
+    // setElementValue
+
+    @Test
+    public void setElementValueShouldSetValueToInputElement() {
+        String value = "this is new value";
+        steps.shouldNotHaveValue(inputId, value);
+        steps.setElementValue(value, inputId);
+        steps.shouldHaveValue(inputId, value);
+    }
+
+    @Test
+    public void setElementValueShouldClearValueBeforeSettingTheNewOne() {
+        String value = "this is new value";
+        steps.setElementValue("some random value", inputId);
+        steps.setElementValue(value, inputId);
+        steps.shouldHaveValue(inputId, value);
+    }
+
+    // appendElementValue
+
+    @Test
+    public void appendElementValueShouldAppendValueToInputElement() {
+        String value1 = "value1";
+        String value2 = "value2";
+        steps.setElementValue(value1, inputId);
+        steps.shouldHaveValue(inputId, value1);
+        steps.appendElementValue(value2, inputId);
+        steps.shouldHaveValue(inputId, value1 + value2);
     }
 
 }
